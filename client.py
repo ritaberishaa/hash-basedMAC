90c953b5d835d19f1fdd6094501dae479b7c414b
ritaberishaa <76747778+ritaberishaa@users.noreply.github.com>
2024-05-20 15:10:52 +0200

    comments of functionality for each part

--- client.py
+++ client.py
@@ -4,13 +4,10 @@ import logging
 import os
 import socket
 
-# Setup i logging
+# konfigurohet sistemit i regjistrimit te logjeve per te ruajtur logjet ne nje file specifik 
 logging.basicConfig(filename='client.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# gjithashtu edhe shared secret key
-#fillimisht n terminal para se me bo cd to this folder, shumbull: export SHARED_SECRET_KEY=qelesi123
-#edhe duhet me u bo export ne te dy terminalet
-
+# kontrollohet dhe merret celesi i perbashket nga environment variable, nese ky celes nuk vendoset programi ndalon ekzekutimin dhe jep nje mesazh gabimi
 shared_secret_key = os.getenv('SHARED_SECRET_KEY')
 if shared_secret_key is None:
     logging.error("SHARED_SECRET_KEY environment variable not set.")
@@ -18,21 +15,26 @@ if shared_secret_key is None:
     exit(1)
 shared_secret_key = shared_secret_key.encode()
 
+# krijohet nje socket klienti dhe lidhet me serverin ne adresen edhe portin e specifikuar
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
+# perdoruesi jep mesazhin, klienti gjeneron nje HMAC per ate mesazh duke perdor celesin dhe funksionin hash SHA-256
 message = input("Enter your message: ")
 hmac_signature = hmac.new(shared_secret_key, message.encode(), hashlib.sha256).digest()
 
+# mesazhi dhe HMAC dergohen te serveri ne formatin e specifikuar
 client_socket.send(len(message).to_bytes(4, 'big'))
 client_socket.send(message.encode())
 client_socket.send(hmac_signature)
 
+# klienti pret pergjigjen nga serveri, e dekodon ate dhe e regjistron ne log dhe poashtu shfaqet ne ekran
 logging.info(f"Sending message with HMAC: [{message} | {hmac_signature.hex()}]")
 print(f"Sending message with HMAC: [{message} | {hmac_signature.hex()}]")
-response = client_socket.recv(1024).decode()  # Make sure to receive enough bytes
+response = client_socket.recv(1024).decode()  
 logging.info("Server response: " + response)
 print("Server response:", response)
 
+# ne fund, socketi i klientit mbyllet per te liru burimet
 client_socket.close()
--- server.py
+++ server.py
@@ -3,11 +3,11 @@ import hmac
 import logging
 import os 
 import socket
-# Setup i logging
+
+# konfigurohet sistemit i regjistrimit te logjeve per te ruajtur logjet ne nje file specifik 
 logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-# marrja e shared secret key prej environment variable
+# kontrollohet dhe merret celesi i perbashket nga environment variable, nese ky celes nuk vendoset programi ndalon ekzekutimin dhe jep nje mesazh gabimi
 shared_secret_key = os.getenv('SHARED_SECRET_KEY')
 if shared_secret_key is None:
     logging.error("SHARED_SECRET_KEY environment variable not set.")
@@ -15,35 +15,36 @@ if shared_secret_key is None:
     exit(1)
 shared_secret_key = shared_secret_key.encode()
 
+# funksioni qe gjeneron nje HMAC per mesazhin e pranuar dhe e krahason ate me HMAC e pranuar per te verifiku integritetin dhe autenticitetin e mesazhit
 def verify_hmac(received_message, received_hmac):
     calculated_hmac = hmac.new(shared_secret_key, received_message, hashlib.sha256).digest()
     return hmac.compare_digest(calculated_hmac, received_hmac)
 
-# setup per serverin
+# krijohet nje socket server dhe lidhet ne adresen dhe portin e specifikum
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 server_socket.bind(server_address)
 server_socket.listen(1)
-#per logging edhe printim
+# per logging edhe printim
 logging.info("Server started and awaiting messages...")
 print("Server started and awaiting messages...")
 
 while True:
-    #pranon lidhjet e klienteve
+    # pranon lidhjet e klienteve dhe regjistron adresen e klientit
     client_socket, client_address = server_socket.accept()
     logging.info(f"Connection from {client_address}")
     try:
-        #merr mesazhin
+        # merr mesazhin
         message_length = int.from_bytes(client_socket.recv(4), 'big')
         received_message = client_socket.recv(message_length)
         received_hmac = client_socket.recv(32)
         
-        #printon mesazhin e marre 
+        # printon mesazhin e marre 
         logging.info(f"Message received with HMAC: [{received_message.decode()} | {received_hmac.hex()}]")
         print(f"Message received with HMAC: [{received_message.decode()} | {received_hmac.hex()}]")
         print("Validating HMAC...")
 
-        #verifikon HMAC
+        # verifikon HMAC
         if verify_hmac(received_message, received_hmac):
             response = "Message verified successfully. Integrity and authenticity confirmed."
             logging.info(response)
@@ -55,6 +56,6 @@ while True:
 
         client_socket.send(response.encode())  # me kthy response back to client
 
-    #mbyll lidhjet me klientin
+    # mbyll lidhjet me klientin
     finally:
         client_socket.close()
